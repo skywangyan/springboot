fca0221c32ecb391b884436f607f56db73bf6b9a
yw <jinshiweikai@gmail.com>
2019-10-25 00:09:57 -0700

    initial commit

---
+++ src/main/java/yan/sqliteDemo/SqliteDemoApplication.java
@@ -0,0 +1,14 @@
+package yan.sqliteDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqliteDemoApplication.class, args);
+		System.out.println("java 11 springboot run");
+	}
+
+}
---
+++ src/test/java/yan/sqliteDemo/SqliteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package yan.sqliteDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliteDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
