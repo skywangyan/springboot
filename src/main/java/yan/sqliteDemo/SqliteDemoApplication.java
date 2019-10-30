9d574d3eea7fe0ab44552d914365c6524e4a9d1b
yw <jinshiweikai@gmail.com>
2019-10-29 23:49:19 -0700

    query sqlite locally

--- src/main/java/yan/sqliteDemo/SqliteDemoApplication.java
+++ src/main/java/yan/sqliteDemo/SqliteDemoApplication.java
@@ -1,14 +1,61 @@
 package yan.sqliteDemo;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.*;
 
 @SpringBootApplication
 public class SqliteDemoApplication {
-
+	private Connection conn;
 	public static void main(String[] args) {
 		SpringApplication.run(SqliteDemoApplication.class, args);
 		System.out.println("hello springboot!");
+		SqliteDemoApplication app = new SqliteDemoApplication();
+		app.connect();
+		app.select("PSU");
 	}
 
+	public void connect() {
+		conn = null;
+		try {
+			String url = "jdbc:sqlite:C:/Users/ywang/Dropbox/sqlite/classification.db";
+			conn = DriverManager.getConnection(url);
+			System.out.println("connection established!");
+			select("dummy");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public void select(String description) {
+		if (conn == null) {
+			System.out.println("not connected!");
+			return;
+		}
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM PASSWDS");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				System.out.println(id);
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+
+
+
+
+	}
 }
