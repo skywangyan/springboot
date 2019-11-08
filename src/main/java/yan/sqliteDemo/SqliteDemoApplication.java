4625be3699dca6ca68cc881e8fe699169522c849
yw <jinshiweikai@gmail.com>
2019-11-07 23:32:13 -0800

    rename & refactor

--- src/main/java/yan/sqliteDemo/SqliteDemoApplication.java
+++ src/main/java/yan/sqliteDemo/SqliteDemoApplication.java
@@ -1,81 +1,22 @@
 package yan.sqliteDemo;
-import ch.qos.logback.core.encoder.EchoEncoder;
-import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.sql.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import yan.sqliteDemo.sqlite.Datebase;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SqliteDemoApplication {
-	private Connection conn;
+	@Autowired
+	Datebase sql = null;
 	public static void main(String[] args) {
 		SpringApplication.run(SqliteDemoApplication.class, args);
 		System.out.println("hello springboot!");
-		SqliteDemoApplication app = new SqliteDemoApplication();
-		app.connect();
 	}
-
-	public void connect() {
-		conn = null;
-		try {
-			String url = "jdbc:sqlite:C:/Users/ywang/Dropbox/sqlite/classification.db";
-			conn = DriverManager.getConnection(url);
-			System.out.println("connection established! Please input description key words. ");
-			while (true) {
-				String des = getClientInput();
-				System.out.println(String.format("query description key word : %s", des));
-				select(des);
-			}
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-			}
-		}
+	@PostConstruct
+	void init() {
+		sql.connect();
 	}
-
-	public void select(String description) {
-		if (conn == null) {
-			System.out.println("not connected!");
-			return;
-		}
-		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM PASSWDS WHERE DESCRIPTION LIKE '%%%s%%'", description));
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				String pw = rs.getString("passwd");
-				String username = rs.getString("username");
-				String trueDescirption = rs.getString("description");
-				System.out.println(String.format("| %5d | %80s | %30s | %15s", id, trueDescirption, username, pw));
-
-			}
-			rs.close();
-			stmt.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
-
-
-
-	}
-	private String getClientInput() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String res = "";
-		try {
-			res = br.readLine();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		return res;
-	}
-
 }
---
+++ src/main/java/yan/sqliteDemo/sqlite/Datebase.java
@@ -0,0 +1,74 @@
+package yan.sqliteDemo.sqlite;
+
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.*;
+
+@Component
+public class Datebase {
+    private Connection conn;
+    public void connect() {
+        conn = null;
+        try {
+            String url = "jdbc:sqlite:C:/Users/ywang/Dropbox/sqlite/classification.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("connection established! Please input description key words. ");
+            while (true) {
+                String des = getClientInput();
+                System.out.println(String.format("query description key word : %s", des));
+                select(des);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void select(String description) {
+        if (conn == null) {
+            System.out.println("not connected!");
+            return;
+        }
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM PASSWDS WHERE DESCRIPTION LIKE '%%%s%%'", description));
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String pw = rs.getString("passwd");
+                String username = rs.getString("username");
+                String trueDescirption = rs.getString("description");
+                System.out.println(String.format("| %5d | %80s | %30s | %15s", id, trueDescirption, username, pw));
+
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+    }
+    private String getClientInput() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String res = "";
+        try {
+            res = br.readLine();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
+
+}
